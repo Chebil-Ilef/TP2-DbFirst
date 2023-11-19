4a3b55598a575d14c2549053612e294258cb4521
Chebil-Ilef <ilef.chebil@insat.ucar.tn>
2023-11-19 20:56:17 +0100

    db first done

---
+++ TP2 DbFirst/Models/Genre.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TP2_DbFirst.Models
+{
+    public partial class Genre
+    {
+        public Genre()
+        {
+            Movies = new HashSet<Movie>();
+        }
+
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+
+        public virtual ICollection<Movie> Movies { get; set; }
+    }
+}
---
+++ TP2 DbFirst/Models/TP2DbFirstContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace TP2_DbFirst.Models
+{
+    public partial class TP2DbFirstContext : DbContext
+    {
+        public TP2DbFirstContext()
+        {
+        }
+
+        public TP2DbFirstContext(DbContextOptions<TP2DbFirstContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Genre> Genres { get; set; } = null!;
+        public virtual DbSet<Movie> Movies { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-L9JNVHV;Initial Catalog=TP2DbFirst;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Genre>(entity =>
+            {
+                entity.ToTable("Genre");
+
+                entity.Property(e => e.Id).ValueGeneratedNever();
+            });
+
+            modelBuilder.Entity<Movie>(entity =>
+            {
+                entity.ToTable("Movie");
+
+                entity.HasOne(d => d.Genre)
+                    .WithMany(p => p.Movies)
+                    .HasForeignKey(d => d.GenreId)
+                    .HasConstraintName("FK_Movie_Genre");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
